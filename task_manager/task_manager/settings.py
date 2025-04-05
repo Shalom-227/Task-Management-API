fd63c78591c86807debecde2e693fbb5f4962e11
Shalom-227 <alaladeshalom02@gmail.com>
2025-04-05 07:07:44 +0100

    Update settings.py
    Debug = False
    Review settings for productions environment

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-32b!yllrzilshbte*ai3$trhub08v@twpp$#l7jrg06-(4#ra8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['task-management-api-w7m2.onrender.com']
 
